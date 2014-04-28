63b6ae099c76f502bef227949297e15dd37d11e0
Kenny Lam <kennylam007@hotmail.com>
2014-04-28 16:22:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,77 @@ $('.button').mouseenter(function () {
     $(this).stop().fadeTo('fast', 0.75);
 });
     
-$('.button').mouseleave(function () {
+$(this).mouseleave(function () {
+    
     $(this).stop().fadeTo('fast', 1);
 });
     
-$('.button').click(function () {
-    $(this).hide();
+$('#workButton').click(function () {
+    if($('#work').css('display') === 'none')
+    {
+        $('#workButton').css('opacity', '0.75');
+        $('#work').css('display', 'block');
+    }
+    else
+    {
+        $('#workButton').stop().fadeTo('fast', '1');
+        $('#work').css('display', 'none');
+    }
 });
 
+$('#volunteeringButton').click(function () {
+    if($('#volunteering').css('display') === 'none')
+    {
+        $('#volunteeringButton').css('opacity', '0.75');
+        $('#volunteering').css('display', 'block');
+    }
+    else
+    {
+        $('#volunteeringButton').stop().fadeTo('fast', '1');
+        $('#volunteering').css('display', 'none');
+    }
+});
+
+    
+    $('#activitiesButton').click(function () {
+    if($('#activities').css('display') === 'none')
+    {
+        $('#activitiesButton').css('opacity', '0.75');
+        $('#activities').css('display', 'block');
+    }
+    else
+    {
+        $('#activitiesButton').stop().fadeTo('fast', '1');
+        $('#activities').css('display', 'none');
+    }
+});
+
+    
+    $('#educationButton').click(function () {
+    if($('#education').css('display') === 'none')
+    {
+        $('#educationButton').css('opacity', '0.75');
+        $('#education').css('display', 'block');
+    }
+    else
+    {
+        $('#educationButton').stop().fadeTo('fast', '1');
+        $('#education').css('display', 'none');
+    }
+});
+
+    
+    $('#summaryButton').click(function () {
+    if($('#summary').css('display') === 'none')
+    {
+        $('#summaryButton').css('opacity', '0.75');
+        $('#summary').css('display', 'block');
+    }
+    else
+    {
+        $('#summaryButton').stop().fadeTo('fast', '1');
+        $('#summary').css('display', 'none');
+    }
+});
 
 });
